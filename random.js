7d2c71cb269a2949a494b952d1b0d8830f2644c1
Nathan Briscoe <N.Briscoe1@gmail.com>
2015-12-17 09:49:24 -0600

    here

---
+++ random.js
@@ -0,0 +1,22 @@
+var classMates = [ "natalie", "charlie", "sam", "zach", "nathan", "paul", "anthony", "liz", "eric", "jeremy", "chris", "brooks", "altamir", "amber", "joe", "kenzie", "matthew", "robby", "mark","scott"]
+var groupNumbers = [2,3,4,5,6,7,8,9,10,11]
+
+ //var number of people in group = classMates.length() / groupNumber[i]
+
+ $(document).ready(function(){
+ 	//$('#container').append("<button class = 'groups' >Groups</button");
+
+ 	for(var i = 0; i < groupNumbers.length; i++){
+ 			$('#container').append("<button class = 'groups'>" + groupNumbers[i] + "</button>");
+ 	}
+
+function test (){
+	$(this).
+
+}
+
+
+
+
+
+ });
